6a0ff804feb7211a8df565260bb37c25216ce45c
Alex <eagu616@gmail.com>
2022-09-05 11:30:59 -0500

    primer commit

---
+++ reto.py
@@ -0,0 +1,13 @@
+def run():
+    list_bith = []
+    for i in range(1, 100):
+        if i % 3 != 0:
+          list_bith.append(i**2)
+        
+    print(list_bith)
+
+
+if __name__== "__main__" :
+    run()
+
+    
