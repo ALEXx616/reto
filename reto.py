91c358187631a29c96005be5f86db2bfa7014761
ALEXx616 <112642388+ALEXx616@users.noreply.github.com>
2022-09-29 17:49:27 -0500

    cambio del rango
    cambio de división *2

--- reto.py
+++ reto.py
@@ -1,7 +1,7 @@
 def run():
     list_bith = []
-    for i in range(1, 100):
-        if i % 3 != 0:
+    for i in range(1, 10):
+        if i % 2 != 0:
           list_bith.append(i**2)
         
     print(list_bith)
